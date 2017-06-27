4d886f94699acbdbd99c0f3f5f8f9210657384df
danosaur98 <daniel.zhou2012@gmail.com>
2017-06-27 13:37:55 -0400

    Initial commit

---
+++ CreateExercise.py
@@ -0,0 +1,11 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('Exercises')
+table.put_item(
+    Item={
+        "Name": "overhead press",
+        "MuscleGroup": "shoulder",
+        "HowTo": "https://www.youtube.com/watch?v=F3QY5vMz_6I"
+    }
+)
---
+++ FitFriendPersonalizeHook.py
@@ -0,0 +1,145 @@
+import boto3
+import math
+import dateutil.parser
+import datetime
+import time
+import os
+import logging
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('Users')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+
+
+def get_slots(intent_request):
+    return intent_request['currentIntent']['slots']
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+def delegate(session_attributes, slots):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Delegate',
+            'slots': slots
+        }
+    }
+
+
+""" --- Helper Functions --- """
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    if message_content is None:
+        return {
+            "isValid": is_valid,
+            "violatedSlot": violated_slot,
+        }
+
+    return {
+        'isValid': is_valid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 'content': message_content}
+    }
+
+
+def validate_personalize(name, gender, age, measurementSystem, height, weight, source):
+    return build_validation_result(True, None, None)
+
+
+""" --- Functions that control the bot's behavior --- """
+
+
+def personalize(intent_request):
+    name = get_slots(intent_request)["Name"]
+    gender = get_slots(intent_request)["Gender"]
+    age = get_slots(intent_request)["Age"]
+    measurementSystem = get_slots(intent_request)["MeasurementSystem"]
+    height = get_slots(intent_request)["Height"]
+    weight = get_slots(intent_request)["Weight"]
+    source = intent_request['invocationSource']
+
+    if source == 'DialogCodeHook':
+        # Perform basic validation on the supplied input slots.
+        # Use the elicitSlot dialog action to re-prompt for the first violation detected.
+        slots = get_slots(intent_request)
+
+        validation_result = validate_personalize(name, gender, age, measurementSystem, height, weight, source)
+        if not validation_result['isValid']:
+            slots[validation_result['violatedSlot']] = None
+            return elicit_slot(intent_request['sessionAttributes'],
+                               intent_request['currentIntent']['name'],
+                               slots,
+                               validation_result['violatedSlot'],
+                               validation_result['message'])
+
+    # call to a backend service.
+    return close(intent_request['sessionAttributes'],
+                 'Fulfilled',
+                 {'contentType': 'PlainText',
+                  'content': 'Gottieeeem'})
+
+
+""" --- Intents --- """
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug(
+        'dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'Personalize':
+        return personalize(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
